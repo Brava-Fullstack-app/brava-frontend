7ea8ddd18ae5c71df74b5e24ea92d8bc04216219
nagicome03 <nayeligiselle.0206@gmail.com>
2026-07-23 18:40:06 +0200

    style: font weight fixed in button atom

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,44 +1,11 @@
-import { useState } from 'react';
-import Input from '../shared/components/atoms/Input/Input';
+import Button from '../shared/components/atoms/Button/Button';
 
 export default function App() {
-  const [formData, setFormData] = useState({ email: '', password: '' });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
 
   return (
     <div>
 
-      <Input
-        id="email"
-        name="email"
-        type="email"
-        label="Email"
-        placeholder="tu@email.com"
-        icon="bi-envelope"
-        clearable
-        onClear={() => setFormData({ ...formData, email: '' })}
-        value={formData.email}
-        onChange={handleChange}
-        required
-      />
-
-      <Input
-        id="password"
-        name="password"
-        type="password"
-        label="Contraseña"
-        placeholder="1234Brava"
-        icon="bi-lock"
-        helperText="Mínimo 6 carácteres"
-        clearable
-        onClear={() => setFormData({ ...formData, password: '' })}
-        value={formData.password}
-        onChange={handleChange}
-        required
-      />
+     <Button variant="tertiary">Soy un botón</Button>
     </div>
   );
 }
