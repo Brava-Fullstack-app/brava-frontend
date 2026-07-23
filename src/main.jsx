3f5505e7f89a3d7b5067748b6aec659174eb3a28
nagicome03 <nayeligiselle.0206@gmail.com>
2026-07-23 14:30:06 +0200

    add: variables, mixins and reset style sheets added and index.html fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '../src/app/App'
+import './styles/layout/main.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
